e3bbbaa2d6c2cf8f61f9426ac6be7574cc2c56d8
hisbanshiraz <hisbanbardai@gmail.com>
2023-11-28 16:54:17 -0700

    Implement the search images functionality

--- script.js
+++ script.js
@@ -7,9 +7,32 @@ const showMoreEle = document.querySelector("#show-more-btn");
 
 let page = 1;
 
-function searchImages(searchInput) {
+async function searchImages(searchInput) {
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${searchInput}&client_id=${accessKey}`;
-  console.log(url);
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.results;
+
+  if (results.length) {
+    results.map((result) => {
+      const image = document.createElement("img");
+      image.src = `${result.urls.small}`;
+      image.alt = `${result.alt_description}`;
+      
+      const imageLink = document.createElement("a");
+      imageLink.href = `${result.links.html}`;
+      imageLink.target = `_blank`;
+      imageLink.rel = `noopener noreferrer`;
+      imageLink.text = `${image.alt}`;
+
+      const searchResult = document.createElement("section");
+      searchResult.classList.add('search-result');
+      searchResult.append(image, imageLink);
+      searchResultsEle.appendChild(searchResult);
+
+      showMoreEle.style.display = "block";
+    });
+  }
 }
 
 formEle.addEventListener("submit", (event) => {
