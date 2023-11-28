7ba3c5de3c27be52a0a66026fcdc9540883e61de
hisbanshiraz <hisbanbardai@gmail.com>
2023-11-28 15:34:06 -0700

    Setup searchImages function

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const searchInputEle = formEle.querySelector("input");
 const searchResultsEle = document.querySelector(".search-results");
 const showMoreEle = document.querySelector("#show-more-btn");
 
+let page = 1;
 
+function searchImages(searchInput) {
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${searchInput}&client_id=${accessKey}`;
+  console.log(url);
+}
+
+formEle.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchImages(searchInputEle.value);
+});
