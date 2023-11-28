de859e26c6f67102e760c5d032f7ff4e0b155309
hisbanshiraz <hisbanbardai@gmail.com>
2023-11-28 15:19:22 -0700

    Create all the elements we need in our script

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const accessKey = "U205g-rUhshHJmhSbt-tuJOaRjkcGdlRd4MCOli5-8g";
+
+const formEle = document.querySelector("form");
+const searchInputEle = formEle.querySelector("input");
+const searchResultsEle = document.querySelector(".search-results");
+const showMoreEle = document.querySelector("#show-more-btn");
+
+
